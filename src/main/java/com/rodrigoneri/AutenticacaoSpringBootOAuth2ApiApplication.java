1822b8db2d05e6e9e7cf096169d157ce934139c0
cedro_nds <cedro_nds@192.168.242.99>
2018-05-28 09:43:59 -0300

    commit inicial

---
+++ src/main/java/com/rodrigoneri/AutenticacaoSpringBootOAuth2ApiApplication.java
@@ -0,0 +1,12 @@
+package com.rodrigoneri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacaoSpringBootOAuth2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacaoSpringBootOAuth2ApiApplication.class, args);
+	}
+}
